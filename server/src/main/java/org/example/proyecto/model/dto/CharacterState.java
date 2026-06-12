a5005227da92fb2ab1651c5f81bbe3c5ba9a6f27
gsanmar0604 <gsanmar0604@g.educaand.es>
2026-06-12 17:29:19 +0200

    Combat implemented

---
+++ server/src/main/java/org/example/proyecto/model/dto/CharacterState.java
@@ -0,0 +1,16 @@
+package org.example.proyecto.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CharacterState {
+    private Long characterId;
+    private String name;
+    private String type;
+    private String attributesJson;
+    private String biography;
+}
